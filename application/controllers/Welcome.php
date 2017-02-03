aaa4f2e41e89a5e0359f47e7578808e01fb4f5bf
Jacob Frank <jacobfrank4@gmail.com>
2017-02-02 20:14:17 -0800

    closes #4

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -30,4 +30,19 @@ class Welcome extends Application
 		$this->render();
 	}
 
+	public function shucks()
+	{
+		// this is the view we want shown
+		$this->data['pagebody'] = 'justone';
+
+		// build the list of authors, to pass on to our view
+		$source = $this->quotes->all();	
+
+		//select the last author in the array
+		$record = $source[1];
+
+		$this->data = array_merge($this->data, $record);
+		$this->render();
+	}
+
 }
